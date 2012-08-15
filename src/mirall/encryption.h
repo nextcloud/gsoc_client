c94888b72504844bc806e806e66da9ceb5ccd5ae
Bjoern Schiessle <schiessle@owncloud.com>
2012-08-15 12:49:01 +0200

    - let user enable client side encryption in setup wizard - Encryption class introduced to get/set keys, parse xml, etc.

---
+++ src/mirall/encryption.h
@@ -0,0 +1,52 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+/*
+ * Copyright (C) 2012 Bjoern Schiessle <schiessle@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include <QObject>
+#include <QMap>
+#include <QList>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+class QAuthenticator;
+
+class Encryption : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Encryption(QObject *parent = 0, QString baseurl = "");
+    void setBaseUrl(QString baseurl);
+    void setAuthCredentials(QString username, QString password);
+    void getUserKeys();
+
+private:
+    QString _baseurl;
+    QString _username;
+    QString _password;
+    QNetworkAccessManager *_nam;
+
+    QMap<QString, QString> parseXML(QString xml, QList<QString> tags);
+
+signals:
+    void ocsResults(QMap<QString, QString>);
+
+protected slots:
+    void slotHttpRequestResults(QNetworkReply*);
+    void slotHttpAuth(QNetworkReply*,QAuthenticator*);
+    
+};
+
+#endif // ENCRYPTION_H
