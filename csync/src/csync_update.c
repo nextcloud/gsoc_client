f9b4aa0926e137a274b321e0ba4ba86c7c49e88f
Klaas Freitag <freitag@owncloud.com>
2016-04-20 14:14:05 +0200

    Fix the logging format string.

--- csync/src/csync_update.c
+++ csync/src/csync_update.c
@@ -849,7 +849,7 @@ int csync_ftw(CSYNC *ctx, const char *uri, csync_walker_fn fn,
       flag = CSYNC_FTW_FLAG_NSTAT;
     }
 
-    CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG, "Flag for %s: %ld", filename, flag);
+    CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG, "Flag for %s: %d", filename, flag);
 
     previous_fs = ctx->current_fs;
 
